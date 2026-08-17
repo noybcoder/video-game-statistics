17e0c5d1cb3f04155c1e962a4e2f9225ba1fdca3
Preston Lam <plam@resource-innovations.com>
2026-08-16 19:32:11 -0600

    added create_junction_table and create_direct_link_table methods

--- scripts/poc.py
+++ scripts/poc.py
@@ -1,31 +1,31 @@
 import duckdb, os
 import numpy as np
 
-path = os.path.join(os.getcwd(), 'data/landing/companies_raw_2026-08-14_14-56-18.json')
+path = os.path.join(os.getcwd(), 'data/landing/games_raw_2026-08-14_14-54-44.json')
 conn = duckdb.connect()
 
 entity_name = 'games'
 
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE {entity_name} AS
-#         SELECT
-#             CAST(d.data ->> 'id' AS INTEGER) AS id,
-#             d.data ->> 'name' AS name,
-#             TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
-#             YEAR(release_date) AS release_year,
-#             CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
-#             CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
-#             CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
-#             CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
-#             CAST(d.data ->> 'rating' AS FLOAT) AS rating,
-#             CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
-#             CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
-#             CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
-#             CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
-#             CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
-#         FROM read_json($path, maximum_object_size=60000000) AS g
-#         CROSS JOIN UNNEST(g.data) AS d(data)
-# """, {'path': path})
+conn.execute(f"""
+    CREATE OR REPLACE TABLE {entity_name} AS
+        SELECT
+            CAST(d.data ->> 'id' AS INTEGER) AS id,
+            d.data ->> 'name' AS name,
+            TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
+            YEAR(release_date) AS release_year,
+            CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
+            CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
+            CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
+            CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
+            CAST(d.data ->> 'rating' AS FLOAT) AS rating,
+            CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
+            CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
+            CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
+            CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
+            CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
+        FROM read_json($path, maximum_object_size=60000000) AS g
+        CROSS JOIN UNNEST(g.data) AS d(data)
+""", {'path': path})
 
 # conn.execute(f"""
 #     CREATE OR REPLACE TABLE games_genres AS
@@ -37,28 +37,33 @@ entity_name = 'games'
 #         SELECT id, UNNEST(game_engines) FROM games
 # """)
 
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE games_platforms AS
-#         SELECT id, UNNEST(platforms) FROM games
-# """)
+first_primary_key = 'games'
+second_primary_key = 'genres'
 
-conn.execute(f"""
-    CREATE OR REPLACE TABLE companies AS 
-        SELECT
-            CAST(d.data ->> 'id' AS INTEGER) AS id,
-            d.data ->> 'name' AS name,
-            CAST(d.data ->> 'developed' AS INTEGER[]) AS developed,
-            CAST(d.data ->> 'published' AS INTEGER[]) AS published
-        FROM read_json($path, maximum_object_size=60000000) AS c
-        CROSS JOIN UNNEST(c.data) AS d(data)
-""", {'path': path})
+def create_junction_table(conn, first_primary_key, second_primary_key):
+    table_name = f'{first_primary_key}_{second_primary_key}'
+    conn.execute(f"""
+        CREATE OR REPLACE TABLE {table_name} AS
+            SELECT 
+                id AS {get_singular_entity_name(first_primary_key)}, 
+                UNNEST({second_primary_key}) AS {get_singular_entity_name(second_primary_key)}
+            FROM {first_primary_key}
+    """)
 
-conn.sql(f"""
-    SELECT * FROM companies
-""").show()
+    return conn.table(table_name)
 
-conn.sql("""
-    SELECT table_name, column_name
-    FROM duckdb_columns()
-    WHERE data_type LIKE '%[]' AND (table_name = 'games' OR table_name = 'companies')
-""").show()
+def create_direct_link_table():
+    pass
+
+def get_singular_entity_name(attribute):
+    return f'{attribute[:-1]}_id'
+
+
+create_junction_table(conn, first_primary_key, second_primary_key).show()
+
+
+# conn.sql("""
+#     SELECT table_name, column_name
+#     FROM duckdb_columns()
+#     WHERE data_type LIKE '%[]'
+# """).show()
