b14e951425f1c094defb4a31e9a9e6360481b252
Preston Lam <plam@resource-innovations.com>
2026-08-18 16:23:02 -0600

    still working on the transform part

--- scripts/poc.py
+++ scripts/poc.py
@@ -1,31 +1,43 @@
 import duckdb, os
 import numpy as np
+import inflect
 
-path = os.path.join(os.getcwd(), 'data/landing/games_raw_2026-08-14_14-54-44.json')
+path = os.path.join(os.getcwd(), 'data/landing/genres_raw_2026-08-14_14-54-52.json')
 conn = duckdb.connect()
 
-entity_name = 'games'
+entity_name = 'genres'
 
-conn.execute(f"""
-    CREATE OR REPLACE TABLE {entity_name} AS
-        SELECT
-            CAST(d.data ->> 'id' AS INTEGER) AS id,
-            d.data ->> 'name' AS name,
-            TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
-            YEAR(release_date) AS release_year,
-            CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
-            CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
-            CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
-            CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
-            CAST(d.data ->> 'rating' AS FLOAT) AS rating,
-            CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
-            CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
-            CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
-            CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
-            CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
-        FROM read_json($path, maximum_object_size=60000000) AS g
-        CROSS JOIN UNNEST(g.data) AS d(data)
-""", {'path': path})
+# conn.execute(f"""
+#     CREATE OR REPLACE TABLE {entity_name} AS
+#         SELECT
+#             CAST(d.data ->> 'id' AS INTEGER) AS id,
+#             d.data ->> 'name' AS name,
+#             CAST(d.data ->> 'developed' AS INTEGER[]) as developed,
+#             CAST(d.data ->> 'published' AS INTEGER[]) as published 
+#         FROM read_json($path, maximum_object_size=60000000) AS c
+#         CROSS JOIN UNNEST(c.data) AS d(data)
+# """, {'path': path})
+
+# conn.execute(f"""
+#     CREATE OR REPLACE TABLE {entity_name} AS
+#         SELECT
+#             CAST(d.data ->> 'id' AS INTEGER) AS id,
+#             d.data ->> 'name' AS name,
+#             TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
+#             YEAR(release_date) AS release_year,
+#             CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
+#             CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
+#             CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
+#             CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
+#             CAST(d.data ->> 'rating' AS FLOAT) AS rating,
+#             CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
+#             CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
+#             CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
+#             CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
+#             CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
+#         FROM read_json($path, maximum_object_size=60000000) AS g
+#         CROSS JOIN UNNEST(g.data) AS d(data)
+# """, {'path': path})
 
 # conn.execute(f"""
 #     CREATE OR REPLACE TABLE games_genres AS
@@ -37,10 +49,10 @@ conn.execute(f"""
 #         SELECT id, UNNEST(game_engines) FROM games
 # """)
 
-first_primary_key = 'games'
-second_primary_key = 'genres'
+first_primary_key = 'companies'
+second_primary_key = 'published'
 
-def create_junction_table(conn, first_primary_key, second_primary_key):
+def create_junction_table(conn, first_primary_key: str, second_primary_key: str):
     table_name = f'{first_primary_key}_{second_primary_key}'
     conn.execute(f"""
         CREATE OR REPLACE TABLE {table_name} AS
@@ -52,14 +64,29 @@ def create_junction_table(conn, first_primary_key, second_primary_key):
 
     return conn.table(table_name)
 
-def create_direct_link_table():
-    pass
+def get_singular_entity_name(attribute: str) -> str:
+    p = inflect.engine()
+    singular = p.singular_noun(attribute)
+    if singular:
+        return f'{singular}_id'
+    return f'{attribute}_id'
 
-def get_singular_entity_name(attribute):
-    return f'{attribute[:-1]}_id'
+def create_direct_link_table(conn, entity_name):
+    conn.execute(f"""
+        CREATE OR REPLACE TABLE {entity_name} AS 
+            SELECT
+                CAST(d.data ->> 'id' AS INTEGER) AS id,
+                d.data ->> 'name' AS name
+            FROM read_json($path, maximum_object_size=60000000) AS g
+            CROSS JOIN UNNEST(g.data) AS d(data)
+    """, {'path': path})
+
+    return conn.table(entity_name)
+
+create_direct_link_table(conn, entity_name).show()
 
 
-create_junction_table(conn, first_primary_key, second_primary_key).show()
+# create_junction_table(conn, first_primary_key, second_primary_key).show()
 
 
 # conn.sql("""
