16bd5a80a5a0e39ca221162b057e7e1cd7aad023
Preston Lam <preslam911@gmail.com>
2026-08-18 23:33:37 -0600

    tried something new

--- scripts/poc.py
+++ scripts/poc.py
@@ -1,53 +1,11 @@
 import duckdb, os
 import numpy as np
-import inflect
+import inflect, functools
 
-path = os.path.join(os.getcwd(), 'data/landing/genres_raw_2026-08-14_14-54-52.json')
+path = os.path.join(os.getcwd(), 'data/landing/games_raw_2026-08-14_14-54-44.json')
 conn = duckdb.connect()
 
-entity_name = 'genres'
-
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE {entity_name} AS
-#         SELECT
-#             CAST(d.data ->> 'id' AS INTEGER) AS id,
-#             d.data ->> 'name' AS name,
-#             CAST(d.data ->> 'developed' AS INTEGER[]) as developed,
-#             CAST(d.data ->> 'published' AS INTEGER[]) as published 
-#         FROM read_json($path, maximum_object_size=60000000) AS c
-#         CROSS JOIN UNNEST(c.data) AS d(data)
-# """, {'path': path})
-
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE {entity_name} AS
-#         SELECT
-#             CAST(d.data ->> 'id' AS INTEGER) AS id,
-#             d.data ->> 'name' AS name,
-#             TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
-#             YEAR(release_date) AS release_year,
-#             CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
-#             CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
-#             CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
-#             CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
-#             CAST(d.data ->> 'rating' AS FLOAT) AS rating,
-#             CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
-#             CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
-#             CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
-#             CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
-#             CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
-#         FROM read_json($path, maximum_object_size=60000000) AS g
-#         CROSS JOIN UNNEST(g.data) AS d(data)
-# """, {'path': path})
-
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE games_genres AS
-#         SELECT id, UNNEST(genres) FROM games
-# """)
-
-# conn.execute(f"""
-#     CREATE OR REPLACE TABLE games_game_engines AS
-#         SELECT id, UNNEST(game_engines) FROM games
-# """)
+entity_name = 'games'
 
 first_primary_key = 'companies'
 second_primary_key = 'published'
@@ -71,26 +29,55 @@ def get_singular_entity_name(attribute: str) -> str:
         return f'{singular}_id'
     return f'{attribute}_id'
 
-def create_direct_link_table(conn, entity_name):
-    conn.execute(f"""
+
+def get_base_query(entity_name: str):
+    return f"""
         CREATE OR REPLACE TABLE {entity_name} AS 
             SELECT
                 CAST(d.data ->> 'id' AS INTEGER) AS id,
-                d.data ->> 'name' AS name
-            FROM read_json($path, maximum_object_size=60000000) AS g
-            CROSS JOIN UNNEST(g.data) AS d(data)
-    """, {'path': path})
+                d.data ->> 'name' AS name,
+    """
 
-    return conn.table(entity_name)
+def get_end_query():
+    return f"""
+        FROM read_json($path, maximum_object_size=60000000) AS g
+        CROSS JOIN UNNEST(g.data) AS d(data)
+    """
+    
+def create_direct_link_table(func):
+    @functools.wraps(func)
+    def wrappper_add_query(conn, entity_name, path):
+        query = get_base_query(entity_name)     
+        query += func()
+        query += get_end_query()
 
-create_direct_link_table(conn, entity_name).show()
+        conn.execute(query, {'path': path})
 
+        return conn.table(entity_name)
+    return wrappper_add_query
 
-# create_junction_table(conn, first_primary_key, second_primary_key).show()
+@create_direct_link_table
+def add_game_attributes():
+    return f"""
+        TO_TIMESTAMP(CAST(d.data ->> 'first_release_date' AS BIGINT))::DATE AS release_date,
+        YEAR(release_date) AS release_year,
+        CAST(d.data ->> 'genres' AS INTEGER[]) AS genres,
+        CAST(d.data ->> 'game_engines' AS INTEGER[]) AS game_engines,
+        CAST(d.data ->> 'platforms' AS INTEGER[]) AS platforms,
+        CAST(d.data ->> 'game_type' AS INTEGER) AS game_type,
+        CAST(d.data ->> 'rating' AS FLOAT) AS rating,
+        CAST(d.data ->> 'rating_count' AS INTEGER) AS rating_count,
+        CAST(d.data ->> 'total_rating' AS FLOAT) AS total_rating,
+        CAST(d.data ->> 'total_rating_count' AS INTEGER) AS total_rating_count,
+        CAST(d.data ->> 'aggregated_rating' AS FLOAT) AS aggregated_rating,
+        CAST(d.data ->> 'aggregated_rating_count' AS INTEGER) AS aggregated_rating_count
+    """
 
+@create_direct_link_table
+def add_company_attributes():
+    return f"""
+        CAST(d.data ->> 'developed' AS INTEGER[]) AS game_developed,
+        CAST(d.data ->> 'published' AS INTEGER[]) AS game_published
+    """
 
-# conn.sql("""
-#     SELECT table_name, column_name
-#     FROM duckdb_columns()
-#     WHERE data_type LIKE '%[]'
-# """).show()
+add_game_attributes(conn, entity_name, path).show()
