aa2b9296b075d09e7b00222b4259cb458512a75b
Preston Lam <plam@resource-innovations.com>
2026-09-03 17:28:33 -0600

    really need to work on fastapi stuff

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return [
+        "Top 20 games by rating (from highest to lowest) in each year",
+        "Top 10 genres by rating (from highest to lowest) in each year",
+        "Top 10 in each genre by rating (from highest to lowest) in each year",
+        "Top 10/20 games engines by number of games developed (from highest to lowest) in each year",
+        "Top 10/20 genres by number of games developed (from highest to lowest) in each year",
+        "Top genre by video game developed by year (line chart)",
+        "Top platform by video game developed by year (line chart)"
+    ]
 
 @app.get("/games")
 async def all_games(cur=Depends(get_database_cursor)):
