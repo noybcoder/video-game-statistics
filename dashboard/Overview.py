88edf8d3ffec853ed6e088e2c748fe5af4ba6fb4
Preston Lam <preslam911@gmail.com>
2026-09-17 09:28:08 -0600

    f

--- dashboard/Overview.py
+++ dashboard/Overview.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import plotly.express as px
 
 st.set_page_config(page_title="Overview", page_icon="📊")
 
@@ -26,4 +27,21 @@ def get_data(url):
         return pd.DataFrame()
     
 data = get_data(URL)
-st.table(data)
+
+grouped_data = data \
+    .groupby(by='genre') \
+    .agg(total_game_count=('game_count', 'sum')) \
+    .reset_index() \
+    .sort_values(by='total_game_count', ascending=False)
+
+grouped_data['cumulative_pct'] = grouped_data['total_game_count'].cumsum() / grouped_data['total_game_count'].sum()
+grouped_data['new_genre'] = np.where(grouped_data['cumulative_pct'] > 0.8, 'Others', grouped_data['genre'])
+
+print(grouped_data)
+
+fig = px.pie(names=grouped_data['new_genre'], values=grouped_data['total_game_count'])
+fig.update_traces(textposition='inside', textinfo='label+percent')
+fig.update_layout(margin=dict(t=0, b=0, l=0, r=0), showlegend=False)
+
+
+st.plotly_chart(fig)
