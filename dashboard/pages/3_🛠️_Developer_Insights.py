95cebea16ce0352db6a9da017fccc4526eb6ffcd
Preston Lam <preslam911@gmail.com>
2026-09-18 23:13:28 -0600

    Just one more chat to go

--- dashboard/pages/3_🛠️_Developer_Insights.py
+++ dashboard/pages/3_🛠️_Developer_Insights.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import requests
-import plotly.express as px
 
 st.set_page_config(page_title='Developer Insights', page_icon='🛠️')
 
@@ -26,8 +24,49 @@ def get_data(url):
         return pd.DataFrame()
     
 data = get_data(URL)
-data.rename(inplace=True, columns={
+if data.empty:
+    st.warning("No data available.")
+    st.stop()
+    
+data = data.rename(columns={
     'developer': 'Developer', 'genre': 'Genre', 
-    'total_game_count': 'Number of Games Developed', 'pct_of_total_game_count': 'Percentage of Games Developed'})
+    'total_game_count': 'Number of Games Developed', 'pct_of_total_game_count': 'Percentage of Games Developed'
+})
 
-st.dataframe(data, use_container_width=True, hide_index=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    developer = st.selectbox(
+        'Filter by developer',
+        options=data['Developer'].unique(),
+        index=None,
+        placeholder='Type to find developer...'
+    )
+
+with col2:
+    genres = st.multiselect(
+        'Filter by genres:',
+        options=data['Genre'].unique(),
+        default=None,
+        placeholder='Type to find genres...'
+    )
+
+filtered_data = data.copy()
+
+if developer:
+    filtered_data = filtered_data[filtered_data['Developer'] == developer]
+if genres:
+    filtered_data = filtered_data[filtered_data['Genre'].isin(genres)]
+
+filtered_data = filtered_data.sort_values(by=['Percentage of Games Developed', 'Genre'], ascending=[False, True])
+
+st.subheader('Genre Specialization Among Active Developers')
+st.dataframe(filtered_data, use_container_width=True, hide_index=True, column_config={
+        "Percentage of Games Developed": st.column_config.ProgressColumn(
+            "Percentage of Games Developed",
+            format="%.1f%%",
+            min_value=0,
+            max_value=100,
+        )
+    })
+st.caption("Developers with multiple genres listed are tied for their top genre specialization.")
